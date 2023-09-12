657d0c2985b86986c61b7f1e4c815ffd335058e8
Meenakshi Suresh <119519427+MnkshiSuresh@users.noreply.github.com>
2023-09-12 12:39:38 +0530

    Create Operators.js

---
+++ Operators.js
@@ -0,0 +1,26 @@
+//operators in js
+
+console.log("operators in js")
+let a=23;
+let b=5;
+//arithmetic operators
+console.log("a+b=",a+b)
+console.log("a-b=",a-b)
+console.log("axb=",a*b)
+console.log("a/b=",a/b)
+console.log("a++=",++a)
+console.log("a--=",--a)
+//assignmment operators
+a+=5
+console.log(a)
+//comparison opertors
+console.log("a==b",a==b)
+console.log("a!=b",a!=b)
+console.log("a===b",a===b)
+console.log("a!==b",a!==b)  //type of comparison
+
+//logical operators
+a=22
+console.log(a<b && a==23)
+console.log(a>b && a==22)
+console.log(a>b || a==5)
