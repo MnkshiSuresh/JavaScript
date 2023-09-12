bc03f58d89fc886cd1dbb6ed3781f21aa2fbcf90
Meenakshi Suresh <119519427+MnkshiSuresh@users.noreply.github.com>
2023-09-12 18:51:29 +0530

    Create Functions.js

---
+++ Functions.js
@@ -0,0 +1,15 @@
+function onePlusavg(x, y){
+    console.log("done");
+    return 1 + (x + y) / 2
+}
+
+let a=6
+let b=3
+let c=7
+let a=prompt("enter the value of a")
+let b=prompt("enter the value of b")
+let c=prompt("enter the value of c")
+
+console.log("One plus avg of a and b:"+ onePlusavg(a, b))
+console.log("One plus avg of b and c:"+ onePlusavg(c, b))
+console.log("One plus avg of c and a:"+ onePlusavg(c, a))
