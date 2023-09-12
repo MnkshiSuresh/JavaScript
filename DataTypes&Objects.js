75714c61b21b844d9fcb7a9eb1b9a9fe2acd4d94
Meenakshi Suresh <119519427+MnkshiSuresh@users.noreply.github.com>
2023-09-12 10:38:33 +0530

    Create DataTypes&Objects.js

---
+++ DataTypes&Objects.js
@@ -0,0 +1,23 @@
+//7 Datatypes in js :nn bb ss u
+//; not necessary
+
+let a=null;
+let b=345;
+let c=true;
+let d=BigInt("34")+BigInt("94")
+let e="Harry"
+let f=Symbol("I'm Meenakshi")
+let g=undefined
+console.log(a,b,c,d,e,f,g)
+comsole.log(typeof d)
+
+//object in js
+
+const item= {
+    "harry": true,
+    "mnkshi": "better",
+    "lovish": false,
+    "memee": 67,
+    "shreya": undefined,
+}
+console.log(item["mnkshi"])
