00d455b0e0b1c47e450ba6f0dbeebed51689ea92
Meenakshi Suresh <119519427+MnkshiSuresh@users.noreply.github.com>
2023-09-12 19:11:00 +0530

    Update String.js

--- String.js
+++ String.js
@@ -6,3 +6,14 @@ let boy2= "nandu"
 let sentence=`${boy2} is a frnd of ${boy1}`     // ` used!
 console.log(sentence)
 console.log(boy1.length)
+console.log(boy2.toUpperCase());
+console.log(boy3.toLowerCase());
+console.log(boy2.slice(2,4));
+console.log(boy2.slice(1));
+console.log(boy2.replace("nan","mis"));
+console.log(boy2.concat("is a frnd of ",boy3,"\nOK"));
+let girl="        meena       "
+console.log(girl);
+console.log(girl.trim())
+let girl2="ammu"
+console.log(girl2[2])
