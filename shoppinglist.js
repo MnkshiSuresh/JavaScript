d58357c415837cc642177b3a18a9c4930fb31b96
Meenakshi Suresh <119519427+MnkshiSuresh@users.noreply.github.com>
2023-09-24 11:34:43 +0530

    Create shoppinglist.js

---
+++ shoppinglist.js
@@ -0,0 +1,15 @@
+var shopping_list=[["banana",5],["chocos",2],["pumpkin",1],["flowervase",1],["knife",3]];
+//now i need to cut items i bought in the order of the list:
+
+shopping_list.shift();
+shopping_list.shift();
+
+//when i forgot something previously added in the list:
+
+shopping_list.unshift();
+
+//when i want to add new things to the list:
+shopping_list.push();
+
+//when after shopping i need to delete all the list
+shopping_list.pop();
