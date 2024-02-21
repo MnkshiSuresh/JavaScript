88cc436ad289bd212fd02b9aa55440492506c149
Meenakshi Suresh <119519427+MnkshiSuresh@users.noreply.github.com>
2024-02-21 22:47:17 +0530

    Create RandomNumber.js

---
+++ RandomNumber.js
@@ -0,0 +1,9 @@
+// generating  a random number
+const a = Math.random();
+console.log(a);
+
+//Math.random() * (highestNumber - lowestNumber) + lowestNumber
+// generating a random number btw 1 and 10
+const a = Math.random() * (10-1) + 1
+console.log(`Random value between 1 and 10 is ${a}`);
+
