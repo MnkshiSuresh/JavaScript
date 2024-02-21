2c52eba4887a5ec4ec619a2cdf628343761faae4
Meenakshi Suresh <119519427+MnkshiSuresh@users.noreply.github.com>
2024-02-21 22:34:38 +0530

    Create Conditions.js

---
+++ Conditions.js
@@ -0,0 +1,44 @@
+//if
+
+const a= 23;
+
+if(a){
+  console.log("a has a value");
+}
+
+const b=null;
+if(b){
+  console.log("b has a value");
+}
+else{
+    console.log("no value");
+}
+
+//if-else
+
+const a=1;
+
+if(a==2){
+  console.log("a has a value of 2");
+}
+else{
+console.log("a has another value ie 1");
+}
+
+
+//else-if(ladder)
+
+const a=6;
+
+if(a==20){
+  console.log("a = 20");
+}
+else if(a==50){
+  console.log("a = 50");
+}
+else if(a==56){
+  console.log("a is 56");
+}
+else{
+  console.log("a is having some other value");
+}
